06f2f313a2258e6522ecba5f8af5912ad6a03b13
miriyan254 <miriyan254@gmail.com>
2025-04-19 00:47:40 +0300

    6. Update GameBoard.jsx

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -6,11 +6,21 @@ import Button from './Button';
 function GameBoard({playerName}){
     const [die, setDie] = useState(1);
     const [unsavedScore, setUnsavedScore] = useState(0);
+    const [savedScore, setSavedScore] = useState(0);
 
     function roll(){
         const randomInt = Math.trunc(Math.random() * 6) + 1;
         setDie(randomInt);
-        setUnsavedScore(prev => prev += randomInt);
+        if(randomInt !== 1){
+            setUnsavedScore(prev => prev += randomInt);
+        }else{
+            setUnsavedScore(0); 
+        }
+    }
+
+    function save(){
+        setSavedScore(prev => prev += unsavedScore);
+        setUnsavedScore(0);
     }
 	 
     return (
@@ -19,8 +29,8 @@ function GameBoard({playerName}){
             <DieImg diePoints={die}/>
             <Info info={unsavedScore} />
             <Button handleClick={roll}>Roll</Button>
-            <Info info={0} />
-            <Button>Save</Button>
+            <Info info={savedScore} />
+            <Button handleClick={save}>Save</Button>
         </section>
     );
 }
