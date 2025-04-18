22882a2d1f55e17cdc29da8a8700c477f5f44e29
miriyan254 <miriyan254@gmail.com>
2025-04-18 23:07:54 +0300

    Update DieImg.jsx

--- src/components/DieImg.jsx
+++ src/components/DieImg.jsx
@@ -1,7 +1,25 @@
 import One from '../assets/dice-1.png';
+import Two from '../assets/dice-2.png';
+import Three from '../assets/dice-3.png';
+import Four from '../assets/dice-4.png';
+import Five from '../assets/dice-5.png';
+import Six from '../assets/dice-6.png';
 
-function DieImg(){
-    return <img src={One} />;
+function DieImg({diePoints}){
+    
+    return (
+		<>
+			{ 
+				diePoints === 1 ? <img src={One} /> : 
+				diePoints === 2 ? <img src={Two} /> : 
+				diePoints === 3 ? <img src={Three} /> : 
+				diePoints === 4 ? <img src={Four} /> : 
+				diePoints === 5 ? <img src={Five} /> : 
+				<img src={Six} />
+			} 
+		</>	 
+		
+	);
 }
 
 export default DieImg;
